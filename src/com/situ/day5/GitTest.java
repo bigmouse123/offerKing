ce9854c1a3d66eefedbeff56fba7efc74f193296
lijiankun <1732931774@qq.com>
2025-01-15 19:16:07 +0800

    项目上传GitHub仓库测试

---
+++ src/com/situ/day5/GitTest.java
@@ -0,0 +1,12 @@
+package com.situ.day5;
+
+/**
+ * @author 李建坤
+ * @version 1.0
+ * @date 2025/1/15 19:14
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("项目上传GitHub仓库测试");
+    }
+}
